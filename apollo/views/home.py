1e6c10273f450eeff41198a3b4de5f988dd9cb99
Jade <littleyu.hit@gmail.com>
2015-04-04 15:02:23 +0800

    change

--- apollo/views/home.py
+++ apollo/views/home.py
@@ -2,11 +2,15 @@
 
 from share.framework.bottle import MethodView, view
 
+from apollo.models import MarketModel
+
 
 class HomeView(MethodView):
     @view('index.html')
     def get(self):
-        return {}
+        market = MarketModel.query.first()
+        return {
+            'floor_data': [i.layout.data for i in market.floors.all()]}
 
 
 class AboutView(MethodView):
